771875f40558eac7781422661e1dfb35dfa655da
Xael <97090524+XaelBaseth@users.noreply.github.com>
2023-05-24 14:02:56 +0200

    Update reverse.c

--- src/op/reverse.c
+++ src/op/reverse.c
@@ -17,7 +17,7 @@ void	reverse_rotate_stack_a(long *pile_a, int *nums_in_a)
 	}
 	pile_a[0] = *temp;
 	free(temp);
-	write(1, "Reverse Rotate Stack A\n", 24);
+	write(1, "rra\n", 4);
 }
 
 /*	Rotate element in pile B in descending order. The last element
@@ -37,7 +37,7 @@ void	reverse_rotate_stack_b(long *pile_b, int *nums_in_b)
 	}
 	pile_b[0] = *temp;
 	free(temp);
-	write(1, "Reverse Rotate Stack B\n", 24);
+	write(1, "rrb\n", 4);
 }
 /*	Rotate element in pile B and pile A in descending order. The last 
 	element become the first. */
@@ -62,5 +62,5 @@ void	reverse_rotate_stack_a_and_stack_b(long *pile_a, long *pile_b, int *nums_in
 	}
 	pile_b[*nums_in_b - 1] = *temp;
 	free(temp);
-	write(1, "Reverse Rotate both Stack A and B\n", 35);
+	write(1, "rrr\n", 4);
 }
